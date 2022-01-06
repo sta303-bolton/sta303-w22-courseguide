9c3d257998db86e223be4c84a040b04a3e76b52a
elb0 <liza.bolton@utoronto.ca>
2022-01-06 15:54:18 -0500

    ready for TAs

--- links.R
+++ links.R
@@ -1,3 +1,8 @@
+# Policy
+
+yes_extend_policy = "In the case of a personal illness/emergency, a [declaration can be made](https://forms.office.com/Pages/ResponsePage.aspx?id=JsKqeAMvTUuQN7RtVsVSEOKHUU3SzAJJhmOKjJhDWEpUNTFDSzhZTFlXUzVYMVlNM1FEUTRZMkVWOC4u
+), but must be submitted no more than 3 days after the due date. Extensions may be requested through the same from up to 48 hours before the due date."
+
 # Links
 
 piazza = "https://piazza.com/class/kx47tj4fmy65dg"
@@ -17,6 +22,11 @@ daylight_savings = "Sunday, March 13, 2022"
 
 ## Due dates
 
+mini_portfolio = "Thursday, February 3, 2022 at 3:03 p.m. ET"
+
+portfolio = "Thursday, February 17, 2022 at 3:03 p.m. ET"
+
+
 ### Knowledge basket
 know_survey = "Thursday, January 13, 2022 at 3:03 p.m ET"
 know_survey_link = "https://q.utoronto.ca/courses/253305/quizzes/235067"
@@ -28,6 +38,13 @@ prereq_workshop = "Wednesday, February 2, 2022 in class time"
 
 pd_prop = "Thursday, February 3, 2022 at 3:03 p.m. ET"
 
+pd_evidence = "Thursday, March 31, 2022 at 3:03 p.m. ET"
+
+mini_mixed = "Wednesday, March 9, 2022; assessment window from 8:00 a.m. ET - 8:00 p.m. ET"
+
+mixed = "Wednesday, March 16, 2022; assessment window from 8:00 a.m. ET - 8:00 p.m. ET"
+
+final_project = "Thursday, April 7, 2022 at 3:03 p.m. ET for 2% pt bonus. Submission accepted until Monday, April 11, 2022 at 3:03 p.m."
 
 #### Writing
 m1_c = "Friday, January 14, 2022 at 3:03 p.m ET"
