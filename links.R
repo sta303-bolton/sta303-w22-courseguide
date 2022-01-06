a5833f032045537c0f6f3c87faad4f631e14564c
elb0 <liza.bolton@utoronto.ca>
2022-01-06 13:58:37 -0500

    Adding writing

--- links.R
+++ links.R
@@ -4,10 +4,35 @@ piazza = "https://piazza.com/class/kx47tj4fmy65dg"
 
 announcements = "https://q.utoronto.ca/courses/253305/announcements"
 
-waitlist_form = ""
+waitlist_form = "https://forms.office.com/Pages/ResponsePage.aspx?id=JsKqeAMvTUuQN7RtVsVSEOKHUU3SzAJJhmOKjJhDWEpURUtOV1dVU1hZVjJRQzBHMTk0WEhQTFZXVS4u"
 
-extension = ""
+extension = "https://forms.office.com/Pages/ResponsePage.aspx?id=JsKqeAMvTUuQN7RtVsVSEOKHUU3SzAJJhmOKjJhDWEpUNTFDSzhZTFlXUzVYMVlNM1FEUTRZMkVWOC4u"
+regrade =
+
+markus = "https://markus-ds.teach.cs.toronto.edu/"
 
 # Dates
 
 daylight_savings = "Sunday, March 13, 2022"
+
+## Due dates
+
+### Knowledge basket
+know_survey = "Thursday, January 13, 2022 at 3:03 p.m ET"
+know_survey_link = "https://q.utoronto.ca/courses/253305/quizzes/235067"
+
+prereq_quiz = "Thursday, January 20, 2022 at 3:03 p.m ET"
+prereq_quiz_link = "https://q.utoronto.ca/courses/253305/quizzes/235900"
+
+prereq_workshop = "Wednesday, February 2, 2022 in class time"
+
+pd_prop = "Thursday, February 3, 2022 at 3:03 p.m. ET"
+
+
+#### Writing
+m1_c = "Friday, January 14, 2022 at 3:03 p.m ET"
+m1_a = "Tuesday, January 18, 2022 at 3:03 p.m ET"
+m1_r = "Friday, January 21, 2022 at 3:03 p.m ET"
+
+# m1_link =
+
